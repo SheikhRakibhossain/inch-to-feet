bfe33879b0a0fd2610d4d0a4ad33efe79e32ef03
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2022-03-04 10:13:57 +0600

    first commit

---
+++ inctofit.js
@@ -0,0 +1,34 @@
+var inch = 123;
+var feet = inch / 12;
+console.log(feet);
+console.log(inch);
+console.log(feet);
+
+function inchToFeet(inch) {
+
+    var feet = inch / 12;
+    return feet;
+
+}
+
+var nanafeet = inchToFeet(1333);
+console.log(nanafeet);
+var nanifeet = inchToFeet(5343);
+console.log(nanifeet);
+
+function intofeet(inch) {
+    var feet = inch / 12;
+    return feet;
+
+}
+var nana = intofeet(19);
+console.log(nana);
+
+function convertfeet(inchi) {
+    var confeet = inchi / 600;
+    return confeet;
+
+}
+
+var nani = convertfeet(12344555);
+console.log(nani);
---
+++ random.js
@@ -0,0 +1,61 @@
+// function inchToFeet(inch) {
+
+//     var feet = inch / 12;
+//     return feet;
+
+// }
+// var nanaFeet = inchToFeet(200);
+// console.log(nanaFeet);
+// var naniFeet = inchToFeet(500);
+// console.log(naniFeet);
+
+// var name = 'javed akbar';
+// console.log(name.length);
+// if (name.length > 4) {
+
+//     name = 'mona';
+
+// }
+// console.log(name);
+
+
+/*-------Leap year__--------*/
+
+// const year = 4390;
+// const remainder = year % 4;
+// if (remainder == 0) {
+//     console.log(remainder == 0);
+// } else {
+//     console.log(' Your year is not a lear year !')
+// }
+
+// function isLeapYear(year) {
+//     const remainder = year % 4;
+//     if (remainder == 0) {
+//         return true;
+//     } else {
+//         console.log(' Not a leap year')
+//     }
+
+// }
+// const LeapYear = isLeapYear(4530);
+// console.log(LeapYear);
+
+// const num = [1, 2, 3, 4, 5];
+// let a = 'Bangladesh'
+// const mySet = new Set();
+
+// mySet.add("a");
+// mySet.add("b");
+// mySet.add("c");
+
+// console.log(mySet);
+
+// const letters = new Set(["a", "b", "c", "d"]);
+// let text = '';
+// letters.forEach(function(value) {
+//     text += value;
+
+// });
+// console.log(text);
+// console.log(letters.values());
